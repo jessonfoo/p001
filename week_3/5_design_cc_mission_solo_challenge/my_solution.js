a6b25ac2919e8da3352092e1a43b5e911d539051
Maria Piper <maria@devbootcamp.com>
2014-05-23 12:33:47 -0700

    changed solo challenge

---
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -0,0 +1,37 @@
+// This is a solo challenge
+
+// Your mission description:
+// 
+// 
+// 
+
+// Pseudocode
+// 
+// 
+// 
+// 
+// 
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
